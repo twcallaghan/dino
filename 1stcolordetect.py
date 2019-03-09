7c48c56457c790657b06f63aa937816661914784
Thomas Callaghan <callaghantw@gmail.com>
2019-03-08 22:43:16 -0500

    Testing out color detection, I'm currently thinking that there is a chance this could work but I need to get a more specific color to test with. Red is currently working well but it is too common of a color for people to be wearing. My hope is that tomorrow I can try to integrate the game and the color detection to work together.

--- 1stcolordetect.py
+++ 1stcolordetect.py
@@ -1,25 +1,128 @@
 import cv2
 import numpy as np
+import time
 
+# capturing video through webcam
 cap = cv2.VideoCapture(0)
+starttime = time.time()
+heights = []
+while (1):
+    _, img = cap.read()
 
-while(1):
-    _, frame = cap.read()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([110, 50, 50])
-    upper_red = np.array([130, 255, 255])
+    # converting frame(img i.e BGR) to HSV (hue-saturation-value)
 
-mask = cv2.inRange(hsv, lower_red, upper_red)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-res = cv2.bitwise_and(frame, frame, mask = mask)
-cv2.imshow('frame', frame)
-cv2.imshow('mask', mask)
-cv2.imshow('res', res)
+    # defining the range of red color
+    red_lower = np.array([136, 87, 111], np.uint8)
+    red_upper = np.array([180, 255, 255], np.uint8)
 
-k = cv2.waitKey(5) & 0xFF
-if (k == 27):
-    pass
+    #orange_lower = np.array([255, 127, 80], np.uint8)
+    #orange_upper = np.array([255, 195, 0], np.uint8)
+    '''
+    # defining the Range of Blue color
+    blue_lower = np.array([99, 115, 150], np.uint8)
+    blue_upper = np.array([110, 255, 255], np.uint8)
+    
+    # defining the Range of yellow color
+    yellow_lower = np.array([22, 60, 200], np.uint8)
+    yellow_upper = np.array([60, 255, 255], np.uint8)
+    '''
+    # finding the range of red,blue and yellow color in the image
+    red = cv2.inRange(hsv, red_lower, red_upper)
+    #orange = cv2.inRange(hsv, orange_lower, orange_upper)
+    #blue = cv2.inRange(hsv, blue_lower, blue_upper)
+    #yellow = cv2.inRange(hsv, yellow_lower, yellow_upper)
 
-cv2.destroyAllWindows()
+    # Morphological transformation, Dilation
+    kernal = np.ones((5, 5), "uint8")
 
-cap.release()
+    red = cv2.dilate(red, kernal)
+    res = cv2.bitwise_and(img, img, mask=red)
+
+    #orange = cv2.dilate(orange, kernal)
+    #res1 = cv2.bitwise_and(img, img, mask=orange)
+
+    #blue = cv2.dilate(blue, kernal)
+    #res1 = cv2.bitwise_and(img, img, mask=blue)
+
+    #yellow = cv2.dilate(yellow, kernal)
+    #res2 = cv2.bitwise_and(img, img, mask=yellow)
+
+    # Tracking the Red Color
+    (_, contours, hierarchy) = cv2.findContours(red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for pic, contour in enumerate(contours):
+        endtime = time.time()
+        area = cv2.contourArea(contour)
+        avgavailable = False
+        global contourbool
+        contourbool = False
+        if endtime - starttime > 5:
+            if (area > 50):
+                global y
+                x, y, w, h = cv2.boundingRect(contour)
+                contourbool = True
+                #if y < 150:
+                    #print 'mouth?'
+                #    break
+                #else:
+                    #print y
+                heights.append(y)
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                cv2.putText(img, "RED color", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
+                #print y
+            if len(heights) % 40 == 0:
+                print 'I NOW HAVE ENOUGH POINTS: GO!'
+                a = 0
+                totalheight = 0
+                avgheight = 0.0
+                for height in heights:
+                    totalheight += heights[a]
+                    a += 1
+                    avgheight = totalheight/a
+                #print 'average height ' + str(avgheight)
+                avgavailable = True
+
+            if avgavailable == True and contourbool == True and y > avgheight + 75:
+                print 'duck'
+                print 'y ' + str(y)
+                print 'average height ' + str(avgheight)
+            if avgavailable == True and contourbool == True and  y < avgheight - 75:
+                print 'jump'
+                print 'y ' + str(y)
+                print 'average height ' + str(avgheight)
+    '''
+    (_, contours, hierarchy) = cv2.findContours(orange, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if (area > 50):
+            x, y, w, h = cv2.boundingRect(contour)
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 165, 0), 2)
+            cv2.putText(img, "ORANGE", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 165, 0))
+
+    # Tracking the Blue Color
+    (_, contours, hierarchy) = cv2.findContours(blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if (area > 300):
+            x, y, w, h = cv2.boundingRect(contour)
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.putText(img, "Blue color", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0))
+
+    # Tracking the yellow Color
+    (_, contours, hierarchy) = cv2.findContours(yellow, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if (area > 300):
+            x, y, w, h = cv2.boundingRect(contour)
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(img, "yellow  color", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0))
+    '''
+        # cv2.imshow("Redcolour",red)
+    cv2.imshow("Color Tracking", img)
+    # cv2.imshow("red",res)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
